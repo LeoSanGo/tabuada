f3c17a0ef5306fecd45cb15a9c044e5e7ed0df75
LeoSanGo <souoleo@gmail.com>
2021-06-05 15:03:10 -0300

    Adicionado rotas, controllers e views. Adicionado template bootstrap

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+const indexRouter = require('./routes/index');
 
-app.listen(port, () => console.log("Tabuada on seever port "+port))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+
+app.listen(port, () => console.log('Tabuada on seever port ' + port));
