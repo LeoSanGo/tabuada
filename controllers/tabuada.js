547b7805bb40672a029f06d3905ad7a5c55ebedc
LeoSanGo <souoleo@gmail.com>
2021-06-12 11:11:42 -0300

    Finalizando os testes e adicionando a coverage - instanbul

--- controllers/tabuada.js
+++ controllers/tabuada.js
@@ -1,25 +1,26 @@
 const list = (req, res) => {
-  const numeros = []
+  const numeros = [];
   for (let i = 0; i <= 100; i++) {
-    numeros.push(i)
+    numeros.push(i);
   }
   res.render('tabuada/list', { numeros });
 };
 
 const tabuada = (req, res) => {
-  const tabuadas = []
-  const num = req.params.num
+  const tabuadas = [];
+  const num = req.params.num;
   for (let i = 0; i <= 100; i++) {
     tabuadas.push({
       num,
       i,
-      resultado: num * i
-    })
+      resultado: num * i,
+    });
   }
   res.render('tabuada/tabuada', {
     num: req.params.num,
-    tabuadas
+    tabuadas,
   });
+ 
 };
 
 module.exports = {
